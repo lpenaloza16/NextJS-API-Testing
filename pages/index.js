ddb359a735be66d3660bafa890d6507fe47ee6ad
lpenaloza16 <39505025+lpenaloza16@users.noreply.github.com>
2023-05-25 06:50:09 -0700

    page in

--- pages/index.js
+++ pages/index.js
@@ -5,54 +5,52 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>API Testing Platform</title>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to our API Testing Platform!</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          This platform is dedicated solely to API testing, full-stack testing,
+          Git pulling, and networking.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.card}>
+            <h3>API Testing &rarr;</h3>
+            <p>Conduct rigorous API testing to ensure optimal performance.</p>
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+          <div className={styles.card}>
+            <h3>Full-Stack Testing &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Perform comprehensive full-stack testing for robust applications.
             </p>
-          </a>
+          </div>
+
+          <div className={styles.card}>
+            <h3>Git Pulling &rarr;</h3>
+            <p>
+              Utilize Git pulling for efficient code versioning and team
+              collaboration.
+            </p>
+          </div>
+
+          <div className={styles.card}>
+            <h3>Networking &rarr;</h3>
+            <p>
+              Understand and implement effective networking for your
+              applications.
+            </p>
+          </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://next.new" target="_blank" rel="noopener noreferrer">
+        <p>
           Created with&nbsp;<b>next.new</b>&nbsp;⚡️
-        </a>
+        </p>
       </footer>
     </div>
   );
