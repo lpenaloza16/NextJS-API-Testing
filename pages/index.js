935e0d494acbda35334c4616a162f8daebb895c8
lpenaloza16 <39505025+lpenaloza16@users.noreply.github.com>
2023-05-25 06:53:40 -0700

    revised with purpose

--- pages/index.js
+++ pages/index.js
@@ -5,52 +5,41 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>API Testing Platform</title>
+        <title>API Testing Page</title>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to our API Testing Platform!</h1>
+        <h1 className={styles.title}>Welcome to the API Testing Page</h1>
 
         <p className={styles.description}>
-          This platform is dedicated solely to API testing, full-stack testing,
-          Git pulling, and networking.
+          This page is designed for testing purposes.
         </p>
 
         <div className={styles.grid}>
           <div className={styles.card}>
-            <h3>API Testing &rarr;</h3>
-            <p>Conduct rigorous API testing to ensure optimal performance.</p>
+            <h3>API Testing</h3>
+            <p>Perform API tests for reliability.</p>
           </div>
 
           <div className={styles.card}>
-            <h3>Full-Stack Testing &rarr;</h3>
-            <p>
-              Perform comprehensive full-stack testing for robust applications.
-            </p>
+            <h3>Full-Stack Testing</h3>
+            <p>Carry out full-stack tests for application dependability.</p>
           </div>
 
           <div className={styles.card}>
-            <h3>Git Pulling &rarr;</h3>
-            <p>
-              Utilize Git pulling for efficient code versioning and team
-              collaboration.
-            </p>
+            <h3>Git Pulling</h3>
+            <p>Use Git pulling for code versioning and collaboration.</p>
           </div>
 
           <div className={styles.card}>
-            <h3>Networking &rarr;</h3>
-            <p>
-              Understand and implement effective networking for your
-              applications.
-            </p>
+            <h3>Networking</h3>
+            <p>Understand networking for application interaction.</p>
           </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <p>
-          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
-        </p>
+        <p>Created by Leo Pena ⚡️</p>
       </footer>
     </div>
   );
